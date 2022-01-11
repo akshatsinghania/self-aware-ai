87fd264172dd60208ccf38c6c9ccf3cb846f5c13
akshatsinghania <singhaniaakshat1@gmail.com>
2022-01-11 21:10:09 +0530

    added perceptron

--- main.py
+++ main.py
@@ -1,49 +1,109 @@
-from random import random
+'''this is a neuron
+holds links to other neurons
+modifies link strengths according to conditions
+'''
+import random
+import time
+
+from tqdm import tqdm
+import numpy as np
 
 
-class Node:
-    def __init__(self, info, excite, values):
-        self.info = info
-        self.excite = excite
-        self.value = self.getValue(values)
-
-    def getValue(self, values):
-        if (len(values) == 0):
-            return random()
-        sum = 0
-        for i in values:
-            sum += i.value
-        return sum / len(values)
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
-last_layer = []
-layers_number = 20
-layers = []
-appeared = {}
-for i in range(1, layers_number):
-    current_layer = []
-    if round(layers_number / (2 * i)) in appeared:
-        break
-    appeared[round(layers_number / (2 * i))] = 1
-    for j in range(1, round(layers_number / (2 * i)) + 2):
-        last=0
-        offset=1
-        if i>1:
-            last = (2 * (i - 1))
-            offset=round(layers_number/(j+last))
-        previous = offset - 1
-        nextt = offset + 1
-        print(previous,nextt,end='\t\t')
+class Brain:
+    def __init__(self, size, input_size, output_size, num_synapses=None, density=None):
+        assert num_synapses or density
+        assert not (num_synapses and density)
 
-        if i > 1:
-            current_layer.append(Node(random(), random(), [last_layer[previous], last_layer[nextt]]))
-        else:
-            current_layer.append(Node(random(), random(), []))
-    layers.append(current_layer)
-    last_layer = layers[len(layers) - 1]
-    print()
+        self.size = size
+        self.input_size = input_size
+        self.output_size = output_size
+        self.density = density
+        self.num_synapses = num_synapses
+        if density:
+            self.num_synapses = int(density * size)
+        self.num_synapses = num_synapses if not density else int(density * size)
 
-# for x in layers:
-#     for y in x:
-#         print(round(y.value,ndigits=3),end='\t')
-#     print('\n')
+        self.input_neurons = np.random.randint(0, size, input_size, dtype=int)
+        self.output_neurons = np.random.randint(0, size, output_size, dtype=int)
+
+        self.neurons = np.arange(size, dtype=float)
+
+        #print(f"num_synapses: {self.num_synapses}")
+        #print(f"synaptic size: {size * self.num_synapses})")
+        self.synapses = np.random.randint(0, size, (size, self.num_synapses), dtype=int)
+        self.strengths = np.ones((size, self.num_synapses), dtype=float)
+        self.hebbians = np.zeros((size, self.num_synapses), dtype=float)
+
+    def step(self):
+        self.step_neurons()
+        self.step_synapses()
+
+    def step_neurons(self):
+        self.neurons = sigmoid(np.sum(self.strengths, axis=1) / self.num_synapses)
+
+    def step_synapses(self):
+        self.strengths = self.hebbians * (self.neurons + self.neurons[self.synapses].T).T / 2
+
+    def store(self, data):
+        self.neurons[self.input_neurons] = data
+
+    def load(self):
+        return self.neurons[self.output_neurons]
+
+    def update_hebbians(self,value):
+        self.hebbians=value
+
+    def __repr__(self) -> str:
+        return f"Brain: size: {self.size}, density: {self.density}"
+
+
+class Perceptron:
+    def __init__(self, host, target):
+        self.host=host
+        self.target=target
+        self.value=(target.hebbians+host.hebbians)/2
+
+    def recalculate(self,host,target):
+        self.host=host
+        self.target=target
+        new_value = (host.hebbians+target.hebbians)/2
+        current_value = (new_value+self.value)/2
+        self.value=current_value
+
+
+if __name__ == "__main__":
+    # brain = Brain(size=8, num_synapses=4, input_size=4, output_size=4)
+    # print(brain)
+    #
+    # data = np.random.random(brain.input_size)
+    # print(data)
+    # brain.store(data)
+    # for _ in range(10):
+    #     brain.step()
+    #     print(brain.load())
+
+
+
+    #Perceptron
+    brains=[Brain(size=8, num_synapses=4, input_size=4, output_size=4),Brain(size=8, num_synapses=4, input_size=4, output_size=4)]
+
+    host_data = np.random.random(brains[0].input_size)
+    target_data = np.random.random(brains[0].input_size)
+
+    brains[0].store(host_data)
+    brains[1].store(target_data)
+    for _ in range(10):
+        brains[0].step()
+        brains[1].step()
+
+    perceptron = Perceptron(brains[0],brains[0])
+
+    print(perceptron.value)
+    brains[0].update_hebbians(np.ones((brains[0].size, brains[0].num_synapses), dtype=float))
+    perceptron.recalculate(brains[0],brains[1])
+    print(perceptron.value)
+
